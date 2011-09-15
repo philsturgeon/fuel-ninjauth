af7089eab7f2e499985229c584358edce2b68957
Phil Sturgeon <email@philsturgeon.co.uk>
2011-09-15 21:15:55 +0100

    Removed testing API data.

--- config/ninjauth.php
+++ config/ninjauth.php
@@ -21,32 +21,33 @@ return array(
 	'providers' => array(
 		
 		'facebook' => array(
-			'id' => '192553490816292',
-			'secret' => 'fdd9b8331d0ebec2c939e3dd6bb4e7ec',
+			'id' => '',
+			'secret' => '',
+			'scope' => '',
 		),
 		
 		'twitter' => array(
-			'key' => 'rTEn9REHxgE4OOrAva8mw',
-			'secret' => 'FpQCqRQlZKNOYtcFMYI9RMcpeKF1p8AKNKcz9VL56E',
+			'key' => '',
+			'secret' => '',
 		),
 
 		'dropbox' => array(
-			'key' => 'vvvq4goeycjzfgm',
-			'secret' => 'yi2q4ce0adbcv2v',
+			'key' => '',
+			'secret' => '',
 		),
 
 		'linkedin' => array(
-			'key' => '8bpcipr0v68e',
-			'secret' => 'Ba2MxtNMap7CywKB',
+			'key' => '',
+			'secret' => '',
 		),
 
 		'flickr' => array(
-			'key' => '68afd60a06c12657c324ea83d4c554eb',
-			'secret' => '09dbb56a3d238876',
+			'key' => '',
+			'secret' => '',
 		),
 
 		'youtube' => array(
-			'key' => 'AI39si5U6RpTiDC9l5QDV1Xq9uAYph0PkyXBjDVCf_K4X5nRNMgVMP9d1OTlgDT9KXE-2Qce5AS5UTsdw2SnC0Qvoaqus49QLw',
+			'key' => '',
 			'scope' => 'http://gdata.youtube.com',
 		),
 	
