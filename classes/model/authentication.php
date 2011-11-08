b43ad86fc78c7aee81f85b85ca1bc55febac13e3
Phil Sturgeon <email@philsturgeon.co.uk>
2011-11-08 14:19:19 +0000

    Listed fields in NinjAuth\Model_Authentication for speed and PDO compatability.

--- classes/model/authentication.php
+++ classes/model/authentication.php
@@ -2,4 +2,8 @@
 
 namespace NinjAuth;
 
-class Model_Authentication extends \Orm\Model {}
+class Model_Authentication extends \Orm\Model {
+	
+	protected static $_properties = array('id', 'user_id', 'provider', 'uid', 'token', 'secret', 'created_at', 'updated_at');
+	
+}
