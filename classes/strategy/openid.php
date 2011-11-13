27a4007c02fab8e6f42deaf7f3f748c92a1fab47
Gilles Meier <krtek4@gmail.com>
2011-11-13 10:47:27 -0500

    Brackets style

--- classes/strategy/openid.php
+++ classes/strategy/openid.php
@@ -7,7 +7,8 @@ namespace NinjAuth;
  * The library can be found at http://gitorious.org/lightopenid and must
  * be accessible through the global namespace
  */
-class Strategy_OpenId extends Strategy {
+class Strategy_OpenId extends Strategy
+{
 	/**
 	 * @var LightOpenID our OpenId library instance
 	 */
@@ -17,7 +18,8 @@ class Strategy_OpenId extends Strategy {
 	 * Create the LightOpenId instance.
 	 * @param string $provider The provider name, in our case openid
 	 */
-	public function __construct($provider) {
+	public function __construct($provider)
+	{
 		parent::__construct($provider);
 		$this->openid = new \LightOpenID(\Input::server('HTTP_HOST'));
 	}
@@ -28,19 +30,25 @@ class Strategy_OpenId extends Strategy {
 	 * The post variable set in the configuration must contains the OpenId
 	 * identity !
 	 */
-	public function authenticate() {
+	public function authenticate()
+	{
 		$identity = \Input::post(\Config::get('ninjauth.providers.openid.'));
 		if(empty($url))
+		{
 			exit('Must provide an OpenId identity.');
+		}
 
 		$this->openid->identity = $identity;
 		$this->openid->required = \Config::get('ninjauth.providers.openid.ax_required');
 		$this->openid->optional = \Config::get('ninjauth.providers.openid.ax_optional');
 		$this->openid->returnUrl = \Uri::create(\Config::get('ninjauth.urls.callback', \Request::active()->route->segments[0].'/callback').'/'.$this->provider);
 
-		try {
+		try
+		{
 			header('Location: '.$this->openid->authUrl());
-		} catch(Exception $e) {
+		}
+		catch(Exception $e)
+		{
 			exit('Something went wrong: '.$e);
 		}
 		exit(); // must exit here since we do a redirection.
@@ -51,11 +59,16 @@ class Strategy_OpenId extends Strategy {
 	 * to continue the login process
 	 * @return mixed
 	 */
-	public function callback() {
+	public function callback()
+	{
 		if($this->openid->mode == 'cancel')
+		{
 			exit('user canceled');
+		}
 		if (! $this->openid->validate())
+		{
 			exit('Error');
+		}
 
 		return (object) array(
 			'token' => $this->openid->identity,
@@ -78,13 +91,20 @@ class Strategy_OpenId extends Strategy {
 	 * @param array $data The array containing the OpenId data
 	 * @return string content of the given indexe(s) or ''
 	 */
-	private function get_data($map, $data) {
+	private function get_data($map, $data)
+	{
 		$r = '';
 		if(is_array($map))
+		{
 			foreach($map as $m)
+			{
 				$r .= $this->get_data($m, $data);
+			}
+		}
 		else if(array_key_exists($map, $data))
+		{
 			$r = $data[$map];
+		}
 		return $r;
 	}
 
@@ -92,7 +112,8 @@ class Strategy_OpenId extends Strategy {
 	 * @param mixed $response The response returned by callback()
 	 * @return array Relevent information to create a new authentication entry
 	 */
-	public function get_user_info($response) {
+	public function get_user_info($response)
+	{
 		$ret = array(
 			'credentials' => array(
 				'uid' => $this->openid->identity,
@@ -104,7 +125,9 @@ class Strategy_OpenId extends Strategy {
 
 		$data = $this->openid->getAttributes();
 		foreach(static::$mapping as $name => $map)
+		{
 			$ret[$name] = $this->get_data($map, $data);
+		}
 
 		return $ret;
 	}
