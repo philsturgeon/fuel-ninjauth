1738eca96db679041dda0aa8895c2c5ea7801574
Phil Sturgeon <email@philsturgeon.co.uk>
2012-05-25 10:53:01 +0100

    Updated view handling for 1.2.

--- classes/controller.php
+++ classes/controller.php
@@ -7,6 +7,7 @@ use Config;
 use Input;
 use Response;
 use Session;
+use View;
 
 /**
  * NinjAuth Controller
@@ -122,7 +123,7 @@ class Controller extends \Controller
 			}
 		}
 		
-		$this->response->body = \View::forge('register', array(
+		return View::forge('register', array(
 			'user' => (object) compact('username', 'full_name', 'email', 'password')
 		));
 	}
